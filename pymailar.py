a6d5f9b53687da428fecc8ca5ada6045295b80cd
AjxLab <abe12@mccc.jp>
2020-01-07 11:35:37 +0900

    mail送信に成功

---
+++ pymailar.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formatdate
+import yaml
+
+account = yaml.load(open('gmail.yml'), Loader=yaml.SafeLoader)
+
+FROM_ADDRESS = account['address']
+MY_PASSWORD = account['password']
+TO_ADDRESS = 'abe12@mccc.jp'
+SUBJECT = 'GmailのSMTPサーバ経由'
+BODY = 'pythonでメール送信'
+
+
+def create_message(from_addr, to_addr, subject, body):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Date'] = formatdate()
+    return msg
+
+
+def send(from_addr, to_addrs, msg):
+    smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpobj.ehlo()
+    smtpobj.starttls()
+    smtpobj.ehlo()
+    smtpobj.login(FROM_ADDRESS, MY_PASSWORD)
+    smtpobj.sendmail(from_addr, to_addrs, msg.as_string())
+    smtpobj.close()
+
+
+if __name__ == '__main__':
+
+    to_addr = TO_ADDRESS
+    subject = SUBJECT
+    body = BODY
+
+    msg = create_message(FROM_ADDRESS, to_addr, subject, body)
+    send(FROM_ADDRESS, to_addr, msg)
