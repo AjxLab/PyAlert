fbcfa757675685e128a5d59973737526c3df16d6
AjxLab <abe12@mccc.jp>
2020-01-07 11:52:55 +0900

    クラスを用意

--- pymailar.py
+++ pymailar.py
@@ -4,39 +4,44 @@ from email.mime.text import MIMEText
 from email.utils import formatdate
 import yaml
 
-account = yaml.load(open('gmail.yml'), Loader=yaml.SafeLoader)
 
-FROM_ADDRESS = account['address']
-MY_PASSWORD = account['password']
-TO_ADDRESS = 'abe12@mccc.jp'
-SUBJECT = 'GmailのSMTPサーバ経由'
-BODY = 'pythonでメール送信'
+class PyMailer:
+    def __init__(self, address=None, password=None, yaml_file=None):
+        ## -----*----- コンストラクタ -----*----- ##
+        # address・password / yaml file を指定
+        if yaml_file == None:
+            # address・passwordを指定
+            self.__account = {'address': address, 'password': password}
+        else:
+            # yaml fileを指定
+            self.__account = yaml.load(open(yaml_file), Loader=yaml.SafeLoader)
+
+    def send(self, to_addr, subject, body):
+        ## -----*----- メール送信 -----*----- ##
+        # メッセージオブジェクトを生成
+        msg = self.__create_msg(to_addr, subject, body)
+
+        # 送信
+        smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+        smtpobj.ehlo()
+        smtpobj.starttls()
+        smtpobj.ehlo()
+        smtpobj.login(self.__account['address'], self.__account['password'])
+        smtpobj.sendmail(self.__account['address'], to_addr, msg.as_string())
+        smtpobj.close()
 
 
-def create_message(from_addr, to_addr, subject, body):
-    msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = from_addr
-    msg['To'] = to_addr
-    msg['Date'] = formatdate()
-    return msg
+    def __create_msg(self, to_addr, subject, body):
+        ## -----*----- メッセージを生成 -----*----- ##
+        msg = MIMEText(body)
+        msg['Subject'] = subject
+        msg['From'] = self.__account['address']
+        msg['To'] = to_addr
+        msg['Date'] = formatdate()
 
-
-def send(from_addr, to_addrs, msg):
-    smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
-    smtpobj.ehlo()
-    smtpobj.starttls()
-    smtpobj.ehlo()
-    smtpobj.login(FROM_ADDRESS, MY_PASSWORD)
-    smtpobj.sendmail(from_addr, to_addrs, msg.as_string())
-    smtpobj.close()
+        return msg
 
 
 if __name__ == '__main__':
-
-    to_addr = TO_ADDRESS
-    subject = SUBJECT
-    body = BODY
-
-    msg = create_message(FROM_ADDRESS, to_addr, subject, body)
-    send(FROM_ADDRESS, to_addr, msg)
+    mailer = PyMailer(yaml_file='gmail.yml')
+    mailer.send('abe12@mccc.jp', 'テストタイトル', '夏草や兵どもが夢の跡')
